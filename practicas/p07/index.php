d0b1ea5d653fbfa6b9f9e52bc646ab4f8fd10498
kin <kinflrs01@gmail.com>
2024-09-10 14:36:58 -0600

    Commit 3: Descripción de ejercicio 1-4

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -22,7 +22,19 @@
             }
         }
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: impar  par impar</p>
 
+    <h2>Ejercicio 3</h2>
+    <p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+    pero que además sea múltiplo de un número dado. </p>
+
+    <h2>Ejercicio 4</h2>
+    <p>Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’
+    a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner
+    el valor en cada índice.</p>
+    
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
         Name: <input type="text" name="name"><br>
