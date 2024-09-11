2eb9ebfe0ee7b67430710fe2c8ff2b408ed2a0cd
kin <kinflrs01@gmail.com>
2024-09-11 16:25:36 -0600

    Commit 5: Cambios ejercicio uno

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -6,22 +6,21 @@
     <title>Práctica 7</title>
 </head>
 <body>
+
+<!--Se incluye el doc con las funciones-->
+    <?php
+    include_once('C:\xampp\htdocs\tecweb\src\funciones.php');
+    ?>
     <h2>Ejercicio 1</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="get">
         <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-        Número: <input type="number" name="numero"><br>
+        Número: <input type="number" name="numero"><br> 
         <?php
             if(isset($_GET['numero']))
             {
-                $num = $_GET['numero'];
-                if ($num%5==0 && $num%7==0)
-                {
-                    echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-                }
-                else
-                {
-                    echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-                }
+                $numero = $_GET['numero'];
+                echo multiplo5_7($numero);
+
             }
         ?>
     </form>
