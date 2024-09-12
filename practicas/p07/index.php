0eadd0bb08c74bfad9b75854b6d517313f80faa7
kin <kinflrs01@gmail.com>
2024-09-12 16:10:44 -0600

    Commit 6: ejercicio dos completo

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -11,6 +11,7 @@
     <?php
     include_once('C:\xampp\htdocs\tecweb\src\funciones.php');
     ?>
+
     <h2>Ejercicio 1</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="get">
         <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
@@ -28,6 +29,13 @@
     <h2>Ejercicio 2</h2>
     <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
     secuencia compuesta por: impar  par impar</p>
+    <?php
+    $filas = 100;
+    $columnas = 3;
+    echo llenarMatriz($filas, $columnas);
+
+    ?>
+
 
     <h2>Ejercicio 3</h2>
     <p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
