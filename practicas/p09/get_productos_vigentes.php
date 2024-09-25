132c42ae655f99a2b9370d6991935ff595390e38
kin <kinflrs01@gmail.com>
2024-09-24 23:58:29 -0600

    inclusión de caracteres

--- practicas/p09/get_productos_vigentes.php
+++ practicas/p09/get_productos_vigentes.php
@@ -2,36 +2,30 @@
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 	<?php
-	if(isset($_GET['tope'])){
-		$tope = $_GET['tope'];
-	}
-		
+	
+	/** SE CREA EL OBJETO DE CONEXION */
+	@$link = new mysqli('localhost', 'root', 'kin12345', 'marketzone');	
 
-	if (!empty($tope))
+	//caracteres UTF-8
+	$link->set_charset("utf8mb4");
+
+	/** comprobar la conexión */
+	if ($link->connect_errno) 
 	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'kin12345', 'marketzone');	
-
-		//caracteres UTF-8
-		$link->set_charset("utf8mb4");
-
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = 0") ) 
-		{
-			$productos = $result->fetch_all(MYSQLI_ASSOC);
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
+		die('Falló la conexión: '.$link->connect_error.'<br/>');
+			/** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 	}
+
+	/** Crear una tabla que no devuelve un conjunto de resultados */
+	if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = 0") ) 
+	{
+		$productos = $result->fetch_all(MYSQLI_ASSOC);
+		/** útil para liberar memoria asociada a un resultado con demasiada información */
+		$result->free();
+	}
+
+	$link->close();
+	
 	?>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
