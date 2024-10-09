09d7cd69f049ac33491369047f0ab9f91dbdd0d8
kin <kinflrs01@gmail.com>
2024-10-09 00:54:03 -0600

    get_producto_vigentes_v2.php

--- practicas/p09/get_productos_vigentes_v2.php
+++ practicas/p09/get_productos_vigentes_v2.php
@@ -2,7 +2,17 @@
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 	<?php
-	
+
+	// Inicializar productos
+	$productos = array();
+
+	// Verificar si el parámetro 'tope' está presente en la URL
+	if (isset($_GET['tope'])) {
+		$tope = $_GET['tope'];
+	} else {
+		die('Parámetro "tope" no detectado...');
+	}
+		
 	/** SE CREA EL OBJETO DE CONEXION */
 	@$link = new mysqli('localhost', 'root', 'kin12345', 'marketzone');	
 
@@ -16,12 +26,13 @@
 			/** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 	}
 
-	/** Crear una tabla que no devuelve un conjunto de resultados */
-	if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = 0") ) 
-	{
+	/** Usar prepared statements para realizar una consulta segura */
+	if ($stmt = $link->prepare("SELECT * FROM productos WHERE eliminado = 0 AND unidades <= ?")) {
+		$stmt->bind_param("i", $tope);
+		$stmt->execute();
+		$result = $stmt->get_result();
 		$productos = $result->fetch_all(MYSQLI_ASSOC);
-		/** útil para liberar memoria asociada a un resultado con demasiada información */
-		$result->free();
+		$stmt->free();
 	}
 
 	$link->close();
@@ -31,6 +42,91 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Productos por unidades</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	
+		<script>
+			function show(event) {
+				// Obtener el id de la fila donde está el botón presionado
+				var rowId = event.target.parentNode.parentNode.id;
+
+				// Obtener los datos de la fila en forma de arreglo
+				var data = document.getElementById(rowId).querySelectorAll(".row-data");
+
+				// Extraer datos
+				var nombre = data[0].innerHTML;
+				var marca = data[1].innerHTML;
+				var modelo = data[2].innerHTML;
+				var precio = data[3].innerHTML;
+				var unidades = data[4].innerHTML;
+				var detalles = data[5].innerHTML;
+
+				// Mostrar los datos en una alerta
+				alert("Nombre: " + nombre + "\nMarca: " + marca + "\nModelo: " + modelo);
+
+				// Enviar datos al formulario
+				send2form(rowId, nombre, marca, modelo, precio, unidades, detalles);
+			}
+
+			function send2form(rowId, nombre, marca, modelo, precio, unidades, detalles) {
+				var form = document.createElement("form");
+
+				// Crear campos ocultos en el formulario
+				var idIn = document.createElement("input");
+				idIn.type = 'text';
+				idIn.name = 'id';
+				idIn.value = rowId;
+				form.appendChild(idIn);
+
+				// Nombre
+				var nombreIn = document.createElement("input");
+				nombreIn.type = 'text';
+				nombreIn.name = 'nombre';
+				nombreIn.value = nombre;
+				form.appendChild(nombreIn);
+
+				// Marca
+				var marcaIn = document.createElement("input");
+				marcaIn.type = 'text';
+				marcaIn.name = 'marca';
+				marcaIn.value = marca;
+				form.appendChild(marcaIn);
+
+				// Modelo
+				var modeloIn = document.createElement("input");
+				modeloIn.type = 'text';
+				modeloIn.name = 'modelo';
+				modeloIn.value = modelo;
+				form.appendChild(modeloIn);
+
+				// Precio
+				var precioIn = document.createElement("input");
+				precioIn.type = 'text';
+				precioIn.name = 'precio';
+				precioIn.value = precio;
+				form.appendChild(precioIn);
+
+				// Unidades
+				var unidadesIn = document.createElement("input");
+				unidadesIn.type = 'text';
+				unidadesIn.name = 'unidades';
+				unidadesIn.value = unidades;
+				form.appendChild(unidadesIn);
+
+				// Detalles
+				var detallesIn = document.createElement("input");
+				detallesIn.type = 'text';
+				detallesIn.name = 'detalles';
+				detallesIn.value = detalles;
+				form.appendChild(detallesIn);
+
+				// Configurar y enviar el formulario
+				form.method = 'POST';
+				form.action = 'http://localhost/tecweb/practicas/p10/formulario_productos_v3.php';
+				document.body.appendChild(form);
+				form.submit();
+			}
+	</script>
+
 	</head>
 	<body>
 		<h3>PRODUCTOS NO ELIMINADOS</h3>
@@ -50,6 +146,7 @@
 					<th scope="col">Unidades</th>
 					<th scope="col">Detalles</th>
 					<th scope="col">Imagen</th>
+					<th scope="col">Acciones</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -62,7 +159,10 @@
 							<td><?= $producto['precio'] ?></td>
 							<td><?= $producto['unidades'] ?></td>
 							<td><?= utf8_encode($producto['detalles']) ?></td>
-							<td><img src=<?= $producto['imagen'] ?> ></td>
+							<td><img src="<?= $producto['imagen'] ?>" alt="Producto" /></td>
+							<td>
+								<input type="button" value="Modificar" onclick="show(event)" />
+							</td>
 						</tr>
 					<?php endforeach; ?>
 				</tbody>
