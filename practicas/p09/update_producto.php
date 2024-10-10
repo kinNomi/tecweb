6d52eda178cff925c84c45711a20c72d4ad59fc2
kin <kinflrs01@gmail.com>
2024-10-10 09:08:27 -0600

    update_producto.php cambios

--- practicas/p09/update_producto.php
+++ practicas/p09/update_producto.php
@@ -6,27 +6,28 @@ if ($link === false) {
     die("ERROR: No pudo conectarse con la DB. " . mysqli_connect_error());
 }
 
+//verifica que se hayan enviando los datos del formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = mysqli_real_escape_string($link, $_POST['id']);
+    $nombre = mysqli_real_escape_string($link, $_POST['nombre']);
+    $marca = mysqli_real_escape_string($link, $_POST['marca']);
+    $modelo = mysqli_real_escape_string($link, $_POST['modelo']);
+    $precio = mysqli_real_escape_string($link, $_POST['precio']);
+    $unidades = mysqli_real_escape_string($link, $_POST['unidades']);
+    $detalles = mysqli_real_escape_string($link, $_POST['detalles']);
+    $imagen = mysqli_real_escape_string($link, $_POST['imagen']);
+}
 
-// Obtener los valores del formulario
-$idProducto = mysqli_real_escape_string($link, $_POST['id']);
-$nombre = mysqli_real_escape_string($link, $_POST['nombre']);
-$marca = mysqli_real_escape_string($link, $_POST['marca']);
-$modelo = mysqli_real_escape_string($link, $_POST['modelo']);
-$precio = mysqli_real_escape_string($link, $_POST['precio']);
-$unidades = mysqli_real_escape_string($link, $_POST['unidades']);
-$detalles = mysqli_real_escape_string($link, $_POST['detalles']);
-$imagen = mysqli_real_escape_string($link, $_POST['imagen']);
-
-// Verificar si todos los campos requeridos están presentes
+/* Verificar si todos los campos requeridos están presentes
 if(empty($idProducto) || empty($nombre) || empty($marca) || empty($modelo) || empty($precio)){
     die("ERROR: Faltan campos obligatorios.");
 }
-
+*/
 // Preparar la consulta SQL para actualizar el registro
 $sql = "UPDATE productos 
         SET nombre='$nombre', marca='$marca', modelo='$modelo', precio='$precio', 
             unidades='$unidades', detalles='$detalles', imagen='$imagen' 
-        WHERE id='$idProducto'";
+        WHERE id='$id'";
 
 // Ejecutar la consulta
 if(mysqli_query($link, $sql)){
