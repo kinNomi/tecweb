682016887579bf26faa8aff20289a3f4c53b4969
kin <kinflrs01@gmail.com>
2024-10-20 17:18:41 -0600

    Inicio-Cambios en create-Commit final

--- practicas/p11/product_app/backend/create.php
+++ practicas/p11/product_app/backend/create.php
@@ -26,27 +26,5 @@
         echo '[SERVIDOR] Error: No se recibieron datos del producto';
     }
     $conexion->close();
-/*
-        if ($result = $conexion->query("SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' and eliminado = 0")) {
-            if($result->num_rows > 0) {
-                echo '[SERVIDOR] Error: Producto ya registrado';
-            } else {
-                $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES ('{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', '{$jsonOBJ->precio}', '{$jsonOBJ->detalles}', '{$jsonOBJ->unidades}', '{$jsonOBJ->imagen}')";
-                if($conexion->query($sql)) {
-                    echo '[SERVIDOR] Éxito: Producto creado';
-                    echo '<H3>DATOS PRODUCTO: </H3> ';
-                    echo '<P>Nombre: '.$jsonOBJ->nombre.'</P>';
-                    echo '<P>Marca: '.$jsonOBJ->marca.'</P>';
-                    echo '<P>Modelo: '.$jsonOBJ->modelo.'</P>';
-                    echo '<P>Precio: '.$jsonOBJ->precio.'</P>';
-                    echo '<P>Detalles: '.$jsonOBJ->detalles.'</P>';
-                    echo '<P>Unidades: '.$jsonOBJ->unidades.'</P>';
-                } else {
-                    echo '[SERVIDOR] Error: No se pudo crear el producto';
-                }
-            }
-        } 
-        
-    }
-*/
+
 ?>
