2c0e5e3b657fdadefed8b1206efc1ae38d991939
kin <kinflrs01@gmail.com>
2024-10-20 16:27:50 -0600

    Inicio-Validaciones en agregarProducto

--- practicas/p11/product_app/app.js
+++ practicas/p11/product_app/app.js
@@ -86,7 +86,8 @@ function agregarProducto(e) {
     client.onreadystatechange = function () {
         // SE VERIFICA SI LA RESPUESTA ESTÁ LISTA Y FUE SATISFACTORIA
         if (client.readyState == 4 && client.status == 200) {
-            console.log(client.responseText);
+            let respuesta = JSON.parse(client.responseText);
+            alert(respuesta.mensaje);
         }
     };
     client.send(productoJsonString);
