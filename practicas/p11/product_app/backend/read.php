44e6818be21d4cbedaa192c9c10f8a98f4a616ee
kin <kinflrs01@gmail.com>
2024-10-20 14:12:59 -0600

    --Inicio database---

--- practicas/p11/product_app/backend/read.php
+++ practicas/p11/product_app/backend/read.php
@@ -4,34 +4,24 @@
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
     // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['busqueda'])) {
-        $busqueda = $_POST['busqueda'];
+    if( isset($_POST['id']) ) {
+        $id = $_POST['id'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        $busqueda = "%{$busqueda}%";
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
+            // SE OBTIENEN LOS RESULTADOS
+			$row = $result->fetch_array(MYSQLI_ASSOC);
 
-        $query = "SELECT * FROM productos WHERE 
-                  nombre LIKE ? OR 
-                  marca LIKE ? OR 
-                  detalles LIKE ?";
-
-        if($stmt = $conexion->prepare($query)) {
-            $stmt->bind_param("sss", $busqueda, $busqueda, $busqueda);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            
-            while($row = $result->fetch_assoc()) {
-                $product = array();
+            if(!is_null($row)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
                 foreach($row as $key => $value) {
-                    $product[$key] = utf8_encode($value);
+                    $data[$key] = utf8_encode($value);
                 }
-                $data[] = $product;
             }
-            
-            $stmt->close();
-        } else {
+			$result->free();
+		} else {
             die('Query Error: '.mysqli_error($conexion));
         }
-        $conexion->close();
+		$conexion->close();
     } 
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
