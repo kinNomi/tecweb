cb60344815b8028a7a981c97c180ea479c50756f
kin <kinflrs01@gmail.com>
2024-10-20 13:25:23 -0600

    Cambios en create

--- practicas/p11/product_app/backend/create.php
+++ practicas/p11/product_app/backend/create.php
@@ -7,6 +7,37 @@
         // SE TRANSFORMA EL STRING DEL JASON A OBJETO
         $jsonOBJ = json_decode($producto);
         
+        echo '[SERVIDOR] Nombre: '.$jsonOBJ->nombre; 
+
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE nombre='{$jsonOBJ->nombre}' and eliminado=0;") ) 
+		{
+            /** Se extraen las tuplas obtenidas de la consulta */
+			if($result->num_rows > 0){
+                echo "<h1>ERROR: YA se registró ese producto</h1>";
+            }else{
+                //Consulta anterior: $sql = "INSERT INTO productos VALUES (null,'{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";
+                $sql = "INSERT INTO productos(nombre,marca,modelo,precio,detalles,unidades,imagen) VALUES ('{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}')";
+                if ( $conexion->query($sql) ) 
+                {
+                    echo "<h1>Producto agregado:</h1>";
+                    echo "<p>Nombre: $jsonOBJ->nombre</p>";
+                    echo "<p>Marca: $jsonOBJ->marca</p>";
+                    echo "<p>Modelo: $jsonOBJ->modelo</p>";
+                    echo "<p>Precio: $jsonOBJ->precio</p>";
+                    echo "<p>Detalles: $jsonOBJ->detalles</p>";
+                    echo "<p>Unidades: $jsonOBJ->unidades</p>";
+                    echo "<p>Imagen: $jsonOBJ->imagen</p>";
+
+                }
+                else
+                {
+                    echo 'ERROR: no se pudo agregar el producto';
+                }              
+            }
+		}
+    }
+
+    /*
         //SE VALIDA SI EL PRODUCTO YA EXISTE
         $nombre = $jsonOBJ->nombre;
         $checkQuery = "SELECT * FROM productos WHERE nombre = '$nombre' AND eliminado = 0";
