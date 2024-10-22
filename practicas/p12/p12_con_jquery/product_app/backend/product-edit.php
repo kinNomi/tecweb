b37364c6ec91f57cfee48f6a204c7ea9fc034286
kin <kinflrs01@gmail.com>
2024-10-22 12:34:53 -0600

    Cambios en edit

---
+++ practicas/p12/p12_con_jquery/product_app/backend/product-edit.php
@@ -0,0 +1,29 @@
+<?php
+    include_once __DIR__.'/database.php';
+
+    // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
+    $producto = file_get_contents('php://input');
+    
+    if(!empty($producto)) {
+        // SE TRANSFORMA EL STRING DEL JASON A OBJETO
+        $jsonOBJ = json_decode($producto);
+      
+        $sql = "UPDATE INTO productos SET nombre = '{$jsonOBJ->nombre}', marca = '{$jsonOBJ->marca}', modelo = '{$jsonOBJ->modelo}', precio = {$jsonOBJ->precio}, detalles = '{$jsonOBJ->detalles}', unidades = {$jsonOBJ->unidades}, imagen = '{$jsonOBJ->imagen}' WHERE id = {$jsonOBJ->id}";
+        $result = $conexion->query($sql);
+        
+        if($result){
+            $data['status'] =  "success";
+            $data['message'] =  "Producto actualizado";
+        } else {
+            $data['status'] =  "error";
+            $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+        }
+
+        // Cierra la conexion
+        $conexion->close();
+    }
+        
+
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+?>
