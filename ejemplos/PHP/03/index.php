56b420a5bf33b8cbe850701ddc7e6ace73ba3774
kin <kinflrs01@gmail.com>
2024-10-24 17:47:51 -0600

    Ejemplo con namespace en php POO

---
+++ ejemplos/PHP/03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 3</title>
+</head>
+
+<body>
+    <?php
+    use EJEMPLOS\POO\Cabecera2 as Cabecera;  //usamos la clase Cabecera y le cambiamos el nombre a Cabecera
+    require_once __DIR__ . '/Cabecera.php'; //incluimos el archivo Cabecera.php
+
+
+    $cab = new Cabecera('El rincón del programador', 'center', 'https://cs.buap.mx');
+    $cab->graficar();
+    ?>
+</body>
+</html>
