2645fcd2dd5ff7262ee81d7c54e0076880eeaccc
kin <kinflrs01@gmail.com>
2024-10-29 13:48:21 -0600

    Menu ejercicio 06

---
+++ ejemplos/PHP/06/Menu.php
@@ -0,0 +1,42 @@
+<?php
+
+class Menu{
+    private $opciones = NULL;
+    private $direccion;
+
+    public function __construct($dir){
+        $this->opciones = array();
+        $this->direccion = $dir;
+    }
+
+    public function insertar_opcion($op){
+        $this->opciones[] = $op;
+    }
+
+    private function graficar_horizontal(){
+        for($i = 0; $i < count($this->opciones); $i++){
+            //SE INVOCA A METODO DEL OBJETO CONTENIDO EN LA POSICION $i DEL ARREGLO
+            $this->opciones[$i]->graficar();
+            echo '-';
+        }
+
+    }
+
+    private function graficar_vertical(){
+        for($i = 0; $i < count($this->opciones); $i++){
+            //SE INVOCA A METODO DEL OBJETO CONTENIDO EN LA POSICION $i DEL ARREGLO
+            $this->opciones[$i]->graficar();
+            echo '<br>';
+        }
+
+    }
+
+    public function graficar(){
+        if($this->direccion === 'horizontal'){
+            $this->graficar_horizontal();
+        }else{
+            $this->graficar_vertical();
+        }
+    }
+}
+?>
