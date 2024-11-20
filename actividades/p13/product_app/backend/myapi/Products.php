8dfd78630afda698e426f0a2d6bc6d19cd049222
kin <kinflrs01@gmail.com>
2024-11-19 23:11:03 -0600

    Se crea singleByName

--- actividades/p13/product_app/backend/myapi/Products.php
+++ actividades/p13/product_app/backend/myapi/Products.php
@@ -150,6 +150,34 @@ class Products extends DataBase {
         }
     }
 
+    public function singleByName($nombre){
+        if( isset($nombre) ) {
+            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+            if ( $result = $this->conexion->query("SELECT * FROM productos WHERE nombre = {$nombre} AND eliminado = 0") ) {
+                // SE OBTIENEN LOS RESULTADOS
+                $row = $result->fetch_assoc();
+    
+                if(!is_null($row)) {
+                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                    foreach($row as $key => $value) {
+                        $this->data[$key] = $value;
+                    }
+                    $result->free();
+
+                }else {
+                    $this->data = array(
+                        'status' => 'error',
+                        'message' => 'No se encontró el producto con ese nombre'
+                    );
+                }
+            } else {
+                die('Query Error: '.mysqli_error($this->conexion));
+            }
+            $this->conexion->close();
+        }
+    }
+    
+
     public function getData() {
         // SE HACE LA CONVERSIÓN DE ARRAY A JSON
         return json_encode($this->data, JSON_PRETTY_PRINT);
