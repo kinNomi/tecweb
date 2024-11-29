fb3411231e6077409be26136494e6e2167240dd3
kinNomi <kinflrs01@gmail.com>
2024-11-29 16:58:29 -0600

    edit

--- practicas/p13/product_app/backend/product-edit.php
+++ practicas/p13/product_app/backend/product-edit.php
@@ -1,8 +1,9 @@
 <?php
-    use TECWEB\MYAPI\Products;
-    require_once __DIR__.'/myapi/Products.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-    $productos = new Products('marketzone');
+    use TECWEB\MYAPI\Update\Update;
+
+    $productos = new Update('marketzone');
     $productos->edit( json_decode( json_encode($_POST) ) );
     echo $productos->getData();
 ?>
