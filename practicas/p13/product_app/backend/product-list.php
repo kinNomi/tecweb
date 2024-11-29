08b28ce380e621a6a0e4c95c17e88ac6b1609bc1
kinNomi <kinflrs01@gmail.com>
2024-11-29 14:57:47 -0600

    Cambios en backend (encabezados)

--- practicas/p13/product_app/backend/product-list.php
+++ practicas/p13/product_app/backend/product-list.php
@@ -1,8 +1,9 @@
 <?php
-    use TECWEB\MYAPI\Products as Products;
-    require_once __DIR__.'/myapi/Products.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-    $productos = new Products('marketzone');
+    use TECWEB\MYAPI\Read\Read;
+
+    $productos = new Read('marketzone');
     $productos->list();
     echo $productos->getData();
 ?>
